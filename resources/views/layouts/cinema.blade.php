9a0895429f81555e24e470b6fcf2f7a29dff3a8f
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-10-28 12:15:30 +0100

    several other cleanings

--- resources/views/layouts/cinema.blade.php
+++ resources/views/layouts/cinema.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="robots" content="noindex">
-  <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
+    <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" type="text/css">
     <!-- Styles -->
     <link href="{{ asset('css/cinema.css') }}" rel="stylesheet">
 </head>
