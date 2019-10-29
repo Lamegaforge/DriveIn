3d132611944f95254759e1a02562d72f0d2b2e88
DeGraciaMathieu <contact@degracia-mathieu.fr>
2019-10-29 20:56:20 +0100

    Add halloween theme

--- resources/views/halloween.blade.php
+++ resources/views/halloween.blade.php
@@ -3,9 +3,20 @@
 @push('styles')
     <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/cinema.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/halloween.css') }}" rel="stylesheet">
+    <script type="text/javascript">
+    </script>
 @endpush
 
 @section('body')
+
+<img class="spiders right" src="images/halloween/spiderweb-corner-right.png">
+<img class="spiders left" src="images/halloween/spiderweb-corner-left.png">
+
+<header>
+    <img src="images/halloween/logo.png">
+</header>
+
 <div class='twitchWrapper'>
   <div class='twitchStream'>
     <iframe src="https://player.angelthump.com/?channel=ledrivein" style="border: 0;" webkitallowfullscreen allowfullscreen frameborder="no"></iframe>
